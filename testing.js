f3bf63d88900dcd2638a35092cc4379a2ab68c2c
“HarryDodo” <harrydijkema@gmail.com>
2018-10-19 14:29:45 +0200

    jaja

---
+++ testing.js
@@ -0,0 +1,10 @@
+ const vraag0 = "What is always coming, but never arrives?"
+ const vraag1 = "What an be broken, but it never held?"
+ const vraag2 = "What can one catch but is not thrown?"
+ const antw0 = "Tomorrow"
+ const antw1 = "A promise"
+ const antw2 = "A cold"
+ 
+ const arrayVraag = [vraag0, vraag1, vraag2]
+ const arrayAntw = [antw0, antw1, antw2]
+ console.log(`Het antwoord op de vraag ${arrayVraag[0]} is ${arrayAntw[0]}`)
